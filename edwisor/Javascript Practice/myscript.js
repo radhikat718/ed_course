2b8b2270a3a3426ac0bd5551762cbceb22b37e17
ranju <ranju1729@gmail.com>
2020-06-07 16:03:10 +0530

    Modified myscript.js with lessons on declaring Vars

--- edwisor/Javascript Practice/myscript.js
+++ edwisor/Javascript Practice/myscript.js
@@ -1,2 +1,34 @@
-alert("Hello World")
-alert("Veendum Hello World")
+//Variables
+
+//declare strings
+var myMessage = "My Name is Ranjith"
+var myEmail = "someemail@somedomain.com"
+alert(myMessage)
+alert(myEmail)
+
+//declare number
+var skills = 6;
+var users = 256778;
+var rating = 4.345;
+
+alert(skills)
+alert(users)
+alert(rating)
+alert(users + skills + rating)
+
+//boolean
+
+isRrgisteredUser = true;
+isPaidUser = false
+
+alert(isPaidUser)
+
+
+//Undefined
+var noValue;
+alert(noValue)
+
+//Null
+var testNull = null;
+alert(testNull)
+
